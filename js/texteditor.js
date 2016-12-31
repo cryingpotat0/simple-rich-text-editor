033dda3afbece57c23afe288093c105e73038129
raghavanand98 <raghav_anand98@yahooc=.com>
2016-12-31 22:51:56 +0530

    some style changes etc. broken code addition

--- js/texteditor.js
+++ js/texteditor.js
@@ -1,33 +1,36 @@
 var TextEditor = function(containerName) {
+  //HELPER FUNCTIONS
+
+
   //PROTOTYPE FUNCTIONS
   TextEditor.prototype.addCodeBlock = function(language) {
-    this.codeLanguage = language || 'markup';
+    var codeLanguage = language || 'markup';
     // get the selection range (or cursor     position)
     try {
       var range = window.getSelection().getRangeAt(0); 
-
-      // create a span
-      var newElement = document.createElement('div');
-      strippedName = this.name.replace(/\#/g,'').replace(/\./g,'');
+      console.log(range);
+      var strippedName = this.name.replace(/\#/g,'').replace(/\./g,'');
       var id = strippedName +  this.codeblock.count;
+
       this.codeblock.ids.push(id);
       this.codeblock.count = this.codeblock.count + 1;
-      //var newElement = '<div class = "codeblock" id = "' + id + '"></div>';
-      newElement.id = id;
-      //newElement.innerHTML = 'HelloWOrld';
-      range.deleteContents();
-      // place your span
-      range.insertNode(newElement);
-      //$(containerName + ' #editor').append(newElement);
-      var e = $('#'+id);
-      e.height(100);
-      e.css('border', '1px solid red');
-      e.css('border-radius', '20px');
-      e.css('margin', '5px');
 
-      var flask = new CodeFlask;
-      flask.run('#'+ id, {language: 'js'});
-      flask.update('/* Your Code Here */');
+      var newElement = document.createElement('div');
+      newElement.id = id;
+      my_parent = range.startContainer.parentNode.parentNode.id;
+      if(my_parent === "editor" || my_parent === strippedName) {
+        range.deleteContents();
+        range.insertNode(newElement);
+        var code_block = $('#'+id);
+        code_block.height(100);
+        code_block.css('border', '1px solid red');
+        code_block.css('border-radius', '20px');
+        code_block.css('margin', '5px');
+
+        var flask = new CodeFlask;
+        flask.run('#'+ id, {language: ''});
+        flask.update('/* Your Code Here */');
+      }
 
     } catch(e) {
       window.alert('Please place the cursor inside the textbox');
@@ -66,7 +69,12 @@ var TextEditor = function(containerName) {
     p: '<a href="javascript:" data-command="p">P</a>',
     subscript: '<a href="javascript:" data-command="subscript"><i class="fa fa-subscript"></i></a>',
     superscript: '<a href="javascript:" data-command="superscript"><i class="fa fa-superscript"></i></a>',
-    code: '<a href="javascript:" data-command="codeblock"><i class="fa fa-code"></i></a>'
+    /*code:  '<div data-command= "code" class="code-wrapper"><i class="fa fa-code" style="color:#C96;"></i>'
+          +'<div class="code-palette">'
+          +  '</div>'
+          +'</div>',*/
+    code: '<a data-command="code"><i class = "fa fa-code"></i></a>'
+    
 
   }
 
@@ -75,14 +83,28 @@ var TextEditor = function(containerName) {
       '<div class="toolbar">'
         + '</div>'
         + '<div id="editor" contenteditable>'
-      //      + '<div id="my-code-wrapper" style="height: 50px; padding:20px; border: 1px solid black"></div>'
         +   '<p>Try making some changes here. Add your own text or maybe an image.</p>'
         + '</div>'
     );
     return this;
   }
 
-  //LOCAL FUNCTIONS
+  TextEditor.prototype.initCodePalette = function() {
+    var textArea = this;
+    $('.language-link').click(function() {
+
+      var languageMap = {
+        'Javascript': 'js',
+        'HTML/ Markup': 'markup',
+        'CSS' : 'css',
+        'Other': ''
+      }
+        textArea.addCodeBlock(languageMap[$(this).text()]);
+
+    })
+  }
+
+  //LOCAL FUNCTIONS AND VARIABLES
   this.name = containerName;
   this.codeblock = { }
   this.codeblock.count = 0;
@@ -107,6 +129,7 @@ var TextEditor = function(containerName) {
 
   this.show = function() {
 
+    this.initCodePalette();
     if(this.toolbarOptions.indexOf("forecolor") != -1) {
       this.forePalette = $(this.name + ' .fore-palette');
       for (var i = 0; i < this.colorPalette.length; i++) {
@@ -136,9 +159,11 @@ var TextEditor = function(containerName) {
         url = prompt('Enter the link here: ', 'http:\/\/');
         document.execCommand($(this).data('command'), false, url);
       } 
-      if (command == 'codeblock') {
+      if (command == 'code') {
         _this.addCodeBlock();
-      } else {
+
+      }
+      else {
         document.execCommand($(this).data('command'), false, null);
       }
     });
