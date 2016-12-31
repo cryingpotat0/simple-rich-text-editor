9d22e220e291418b737598a4ce43ac04e42735a2
raghavanand98 <raghav_anand98@yahooc=.com>
2016-12-31 22:54:45 +0530

    reverted to working code addition

--- js/main.js
+++ js/main.js
@@ -6,18 +6,3 @@ var textEditor = new TextEditor('.main-container-2');
 textEditor.init(["code", "forecolor", "backcolor", "unlink", "insertimage", "undo"]).show();
 
 
-var createCodeSelector = function() {
-  var languageMap = {
-    'Javascript': 'js',
-    'HTML/ Markup': 'markup',
-    'CSS' : 'css',
-    'Other': ''
-  }
-  var my_elem = $('.code-palette')
-  my_elem.css('width', '100px');
-  for(var language in languageMap) {
-    my_elem.append('<div class = "language-link" >'+language+'</a></div>');
-  }
-}
-
-createCodeSelector();
